04d01db9b025c7f26bcbd51d5caaa54cda50c88f
1adityashetty1 <adityashetty@berkeley.edu>
2017-03-22 21:02:52 -0700

    Update skeleton_contract.sol
    changed to bytes32 to do string demo

--- bin/skeleton_contract.sol
+++ bin/skeleton_contract.sol
@@ -9,21 +9,21 @@ scheme.requestAccess("1", "1" {from: web3.eth.accounts[1], gas: 999999})
 contract Access{
 	address accessers;
 	address adminAddress;
-	bytes8 key;
-	bool contractStatus= false;
+	bytes32 key;
+	bool contractStatus= true;
 
 	///user(id) to file.id) to request
-	mapping(bytes8 => mapping( bytes8 =>request)) public acl;
+	mapping(bytes32 => mapping( bytes32 =>request)) public acl;
 
 	//used for before and after modifier
 	uint timeout;
 	//maps public hash that files are created with
 	mapping(bytes32 => file) hash2file;
 	//maps user.id to other attributes
-	mapping(bytes8 => user) idUsers;
+	mapping(bytes32 => user) idUsers;
 
 	//maps a file.id to other file attributes
-	mapping(bytes8 => file) idFiles;
+	mapping(bytes32 => file) idFiles;
 
 
 		 modifier onlyBy(user _account){
@@ -32,7 +32,7 @@ contract Access{
 	   }
 
 	   modifier onlyIfActive() {
-	     if (contractStatus) throw;
+	     if (contractStatus == false) throw;
 	     _;
 	   }
 
@@ -53,7 +53,7 @@ contract Access{
 
 	struct request{
 		address accesser;
-		bytes8 accesserid;
+		bytes32 accesserid;
 		uint timestamp;
 		bool confirmed;
 		bool granted;	
@@ -61,21 +61,21 @@ contract Access{
 	}
 
 	struct file{
-		bytes8 id;
+		bytes32 id;
 		bytes32 hash;
 	}
 
 	struct user{
-		bytes8 id;
+		bytes32 id;
 		bool isAdmin;
-		bytes8 first;
-		bytes8 last;
+		bytes32 first;
+		bytes32 last;
 		address user_addr;
 
 	}
 
 	//constructor
-	function Access(bytes8 key, bytes8 first, bytes8 last){
+	function Access(bytes32 key, bytes32 first, bytes32 last){
 		accessers = msg.sender;
 		adminAddress = msg.sender; //assumes that the creator of contract is valid admin, can be changed
 		addUser(key, first,last,msg.sender);
@@ -86,7 +86,7 @@ contract Access{
 
 	//write Access for Admins
 
-	function addUser(bytes8 id, bytes8 first, bytes8 last, address user_addr) onlyIfActive onlyByCreator{
+	function addUser(bytes32 id, bytes32 first, bytes32 last, address user_addr) onlyIfActive onlyByCreator{
 	 	if(msg.sender == adminAddress){
 	 	    idUsers[id] = user(id,true,first,last,user_addr);
 	 	}else{
@@ -94,31 +94,31 @@ contract Access{
 	 	}
 	}
 
-	function getUserAddress(bytes8 id) returns (address){
+	function getUserAddress(bytes32 id) returns (address){
 		return idUsers[id].user_addr;
 	}
 
 //modify so non admins have write file priveleges
-	function addFile(bytes8 id, bytes32 public_hash) onlyIfActive onlyByCreator{
+	function addFile(bytes32 id, bytes32 public_hash) onlyIfActive{
 	 
 	 	idFiles[id] = file(id,sha256(public_hash));
 	 	bytes32 access = idFiles[id].hash;
 	 	hash2file[access] = idFiles[id];
 	}
 
-	function getHashFile(bytes8 id) internal returns(bytes32) {
+	function getHashFile(bytes32 id) internal returns(bytes32) {
 		return idFiles[id].hash;
 	}
 
-	function getRequestfromId(bytes8 id,bytes8 fileid) returns(bool) {
+	function getRequestfromId(bytes32 id,bytes32 fileid) returns(bool) {
 		return acl[id][fileid].status;
 	}
 	
-	function getRequestTimeStampfromId(bytes8 id,bytes8 fileid) returns(uint) {
+	function getRequestTimeStampfromId(bytes32 id,bytes32 fileid) returns(uint) {
 		return acl[id][fileid].timestamp;
 	}
 
-	function promote(bytes8 id) onlyByCreator {
+	function promote(bytes32 id) onlyByCreator {
 		idUsers[key].isAdmin = false;
 		idUsers[id].isAdmin = true;
 		adminAddress = idUsers[id].user_addr;
@@ -140,21 +140,38 @@ contract Access{
     return timeout - now;
   }
 
-  function requestAccess(bytes8 user_id, bytes32 public_hash) onlyIfActive returns(bytes8){
+  function requestAccess(bytes32 user_id, bytes32 public_hash) onlyIfActive returns(bytes32){
   		//
-  		if(hash2file[public_hash].hash != bytes32(0x00000000) ){
-  		    file access = hash2file[public_hash];
+  		bytes32 private_hash = sha256(public_hash);
+  		if(hash2file[private_hash].hash != bytes32(0x00000000) ){
+  		    file access = hash2file[private_hash];
   		    user requester = idUsers[user_id];
   			acl[user_id][access.id] = request(msg.sender,user_id,now,false,true,false);
   			return access.id;
   		} else{
-  		    file access1 = hash2file[public_hash];
-  			acl[user_id][access1.id] = request(msg.sender,user_id,now,false,false,false);
-  			return access1.id;
+  		    
+  			return bytes32(0x00000000);
   		}
   }
   
-  function confirmAccess(bytes8 user_id, bytes8 fileid) onlyIfActive {
+  function bytes32ToString(bytes32 x) constant returns (string) {
+    bytes memory bytesString = new bytes(32);
+    uint charCount = 0;
+    for (uint j = 0; j < 32; j++) {
+        byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+        if (char != 0) {
+            bytesString[charCount] = char;
+            charCount++;
+        }
+    }
+    bytes memory bytesStringTrimmed = new bytes(charCount);
+    for (j = 0; j < charCount; j++) {
+        bytesStringTrimmed[j] = bytesString[j];
+    }
+    return string(bytesStringTrimmed);
+}
+  
+  function confirmAccess(bytes32 user_id, bytes32 fileid) onlyIfActive {
       acl[user_id][fileid].confirmed = true;
       if(acl[user_id][fileid].confirmed == true && acl[user_id][fileid].granted ==true ){
           acl[user_id][fileid].status = true;
